628abdf03922dd679938d83969136871d243addf
dobsy89 <d.obrien2@nuigalway.ie>
2018-10-25 12:14:58 +0100

    changed q1

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int a;
+int* b;
+long c;
+double* d;
+char** e;
+
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
-  return 0;
+    printf("Hello assignment1.\n");
+    
+    printf("%lu \n", sizeof(a));
+    printf("%lu \n", sizeof(b));
+    printf("%lu \n", sizeof(c));
+    printf("%lu \n", sizeof(d));
+    printf("%lu \n", sizeof(e));
+    
+    return 0;
 }
