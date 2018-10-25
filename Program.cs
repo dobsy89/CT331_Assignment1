917a54337caf3f29649fdb4aa32a38520f4d4861
dobsy89 <d.obrien2@nuigalway.ie>
2018-10-25 11:43:46 +0100

    updated question 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CT331_Assignment1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
